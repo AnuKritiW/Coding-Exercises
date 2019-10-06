74fa20f14a464df6411bc4495e9024092089898e
Anu Kriti Wadhwa <anu.kriti.w@gmail.com>
2019-10-06 15:48:42 -0500

    Add explanation.

--- Trapping Rain Water II/trapRainWater.py
+++ Trapping Rain Water II/trapRainWater.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         if not heightMap or not heightMap[0]: #if no rows or no columns
             return 0
-        
+
         rows = len(heightMap)
         columns = len(heightMap[0])
 
@@ -79,3 +79,16 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+#explanation taken rom discussion section
+# Steps:
+# 1. First we add all the surrounding to the building heights to a min_heap. Why?
+#    Cuz we want to get the building that decides the level of the water that is held within
+# 2. We pop from the min_heap, to get the least height building, this dictates how much water to add to result/ans
+# 3. For each neighbour of the popped height we can get different in height and add that difference to the answer, as long as that is +ve
+# 4. And then we push the (max(h, height of nbr), r, c), why? The max height dictates how much water will stay for (r, c) nbrs,
+#    so if (r, c) is higher than the max so far, then that (r, c) will dictate how much it's nbrs can hold, so we pass the max along
+# 5. Also, we use the visited array to keep track of all the visited co-ordinates and only visit unvisited nbrs
+
+# Time: O(m * n log(m * n))
+# Space: O(m * n)
